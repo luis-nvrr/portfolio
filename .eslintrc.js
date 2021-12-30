39b989b9677c1b95a4bbb96f4bd49fdccbc39808
Luis Navarro <luisfnvrr@gmail.com>
2021-12-30 00:39:29 -0300

    fix arrow function components

--- .eslintrc.js
+++ .eslintrc.js
@@ -44,5 +44,12 @@ module.exports = {
         tsx: 'never',
       },
     ],
+    'react/function-component-definition': [
+      2,
+      {
+        namedComponents: 'arrow-function',
+        unnamedComponents: 'arrow-function',
+      },
+    ],
   },
 }
