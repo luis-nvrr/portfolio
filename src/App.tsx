e551e8084d4664263ef9f934591d03a3bce85ec5
Luis Navarro <luisfnvrr@gmail.com>
2021-12-29 23:34:02 -0300

    add tailwind example

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,32 @@
 import React, { useState } from 'react'
 import logo from './logo.svg'
-import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount(() => count + 1)}>
+    <div className="flex bg-slate-900 h-screen items-center">
+      <header className="p-6 max-w-sm mx-auto text-slate-100 font-medium space-x-4 space-y-4">
+        <img src={logo} className="object-center" alt="logo" />
+        <p className="font-semibold text-center">Hello Vite + React!</p>
+        <p className="text-center">
+          <button
+            className="bg-sky-500/50 rounded-lg p-2"
+            type="button"
+            onClick={() => setCount(() => count + 1)}
+          >
             count is:
             {count}
           </button>
         </p>
-        <p>
+        <p className="text-slate-500 text-center">
           Edit
-          <code>App.tsx</code>
+          <code> App.tsx </code>
           and save to test HMR updates.
         </p>
-        <p>
+        <p className="text-center">
           <a
-            className="App-link"
+            className="hover:text-cyan-300/80"
             href="https://reactjs.org"
             target="_blank"
             rel="noopener noreferrer"
@@ -32,7 +35,7 @@ function App() {
           </a>
           {' | '}
           <a
-            className="App-link"
+            className="hover:text-cyan-300/80"
             href="https://vitejs.dev/guide/features.html"
             target="_blank"
             rel="noopener noreferrer"
